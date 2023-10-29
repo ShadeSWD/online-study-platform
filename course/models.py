e86e9122a002a81643399ff9232a6fc09866d552
ShadeSWD <seawolfy60@gmail.com>
2023-10-29 16:46:36 +0300

    creation date

--- course/models.py
+++ course/models.py
@@ -4,6 +4,8 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Course(models.Model):
+    created_at = models.DateTimeField(verbose_name='creation date', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='change date', auto_now=True)
     title = models.CharField(max_length=50, verbose_name='название')
     preview = models.ImageField(upload_to='course_images', verbose_name='Превью', **NULLABLE)
     description = models.TextField(verbose_name='Описание')
@@ -17,7 +19,10 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
+    created_at = models.DateTimeField(verbose_name='creation date', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='change date', auto_now=True)
     title = models.CharField(max_length=50, verbose_name='название')
+    course = models.ForeignKey(to=Course, on_delete=models.CASCADE)
     preview = models.ImageField(upload_to='course_images', verbose_name='Превью', **NULLABLE)
     description = models.TextField(verbose_name='Описание')
     video_url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
