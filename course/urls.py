7b7fec4838bceaf13958c94dcbfb8bfebac6a6d0
ShadeSWD <seawolfy60@gmail.com>
2023-10-29 18:37:52 +0300

    lesson CRUD

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,8 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from course.apps import CourseConfig
-from course.views import CourseViewSet
+from course.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = CourseConfig.name
 
@@ -8,5 +10,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ] + router.urls
