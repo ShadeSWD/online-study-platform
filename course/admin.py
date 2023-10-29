bd09d120af426f2a3a19a42353f1cf4c76d6cd3a
ShadeSWD <seawolfy60@gmail.com>
2023-10-29 16:33:54 +0300

    lesson model

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from course.models import Course
+from course.models import Course, Lesson
 
 admin.site.register(Course)
+admin.site.register(Lesson)
--- course/models.py
+++ course/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Course(models.Model):
     title = models.CharField(max_length=50, verbose_name='название')
-    preview = models.ImageField(upload_to='course_images', verbose_name='Превью')
+    preview = models.ImageField(upload_to='course_images', verbose_name='Превью', **NULLABLE)
     description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
@@ -12,3 +14,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=50, verbose_name='название')
+    preview = models.ImageField(upload_to='course_images', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание')
+    video_url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
