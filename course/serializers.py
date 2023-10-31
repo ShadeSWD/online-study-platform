08b1a63a44bd7c48fe2096b26502d55ddd6894e5
ShadeSWD <seawolfy60@gmail.com>
2023-10-31 22:36:27 +0300

    serialiser number of lessons

--- course/serializers.py
+++ course/serializers.py
@@ -3,9 +3,14 @@ from course.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return Lesson.objects.filter(course=obj.pk).count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'title', 'lessons_count', 'description']
 
 
 class LessonSerializer(serializers.ModelSerializer):
