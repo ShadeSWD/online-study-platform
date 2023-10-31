355f185d0a292269de4a508dfcfc5ee3f6449786
ShadeSWD <seawolfy60@gmail.com>
2023-10-31 23:11:02 +0300

    viewset for payments

--- course/views.py
+++ course/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets, generics
-from course.models import Course, Lesson
-from course.serializers import CourseSerializer, LessonSerializer
+from course.models import Course, Lesson, Payment
+from course.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
