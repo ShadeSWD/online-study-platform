288e11a515ba432aab72c9ec9010a78d01b95c95
ShadeSWD <seawolfy60@gmail.com>
2023-10-31 23:56:29 +0300

    filters

--- course/views.py
+++ course/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from course.models import Course, Lesson, Payment
 from course.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
@@ -11,6 +12,9 @@ class CourseViewSet(viewsets.ModelViewSet):
 class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    ordering_fields = ['created_at', ]
+    filterset_fields = ['course', 'payment_method']
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
