049ac58cf6bc2dd04ee2bd63fa8cc457a7c0b18b
ShadeSWD <seawolfy60@gmail.com>
2023-11-04 17:25:51 +0300

    close lesson crud

--- course/views.py
+++ course/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from course.permissions import IsOwnerOrStaff
 from course.models import Course, Lesson, Payment
 from course.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
@@ -20,6 +21,12 @@ class PaymentViewSet(viewsets.ModelViewSet):
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
 
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
+
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
@@ -29,12 +36,15 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff, ]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff, ]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff, ]
