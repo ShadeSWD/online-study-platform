fb4ae2efbaa3b920b97d5eaa8bbbeab6394a355f
ShadeSWD <seawolfy60@gmail.com>
2023-11-04 17:32:50 +0300

    update permissions lesson crud

--- course/permissions.py
+++ course/permissions.py
@@ -1,9 +1,16 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwnerOrStaff(BasePermission):
+class IsStaff(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_staff:
-            return True
+        return request.user.is_staff
 
+
+class IsNotStaff(BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.is_staff
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
         return request.user == view.get_object().owner
--- course/views.py
+++ course/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from course.permissions import IsOwnerOrStaff
+from course.permissions import IsOwner, IsStaff, IsNotStaff
 from course.models import Course, Lesson, Payment
 from course.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
@@ -20,6 +20,7 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsNotStaff]
 
     def perform_create(self, serializer):
         super().perform_create(serializer)
@@ -36,15 +37,15 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff, ]
+    permission_classes = [IsOwner, IsStaff]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff, ]
+    permission_classes = [IsOwner, IsStaff]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff, ]
+    permission_classes = [IsOwner]
