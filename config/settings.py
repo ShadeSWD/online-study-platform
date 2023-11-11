cde490fb8846cd602f6f1e45cdbff89f2e758403
Arina Aleevskaya <seawolfy60@gmail.com>
2023-11-12 00:26:16 +0300

    Homework 5 (#5)
    * add django
    
    * users app
    
    * users admin
    
    * app course
    
    * lesson model
    
    * creation date
    
    * viewset for course
    
    * lesson CRUD
    
    * change user login
    
    * serialiser number of lessons
    
    * payment model
    
    * viewset for payments
    
    * lessons in course serialiser
    
    * filters
    
    * install jwt
    
    * close for anonims
    
    * close lesson crud
    
    * update permissions lesson crud
    
    * permissions for courses
    
    * validate urls
    
    * subscriptions
    
    * pagination
    
    * unittests
    
    * add docs
    
    * add corseheaders
    
    * add stripe

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'drf_yasg',
+    'corsheaders',
+
 
     'users',
     'course'
@@ -54,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -152,3 +156,15 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://read-only.example.com",
+    "https://read-and-write.example.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://read-and-write.example.com",
+]
+
+# Key for stripe
+STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
